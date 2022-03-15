3238646f11d2e3a5ef5388b6cb06cfe0570352ce
henriquechagasg <henriquechagasg@gmail.com>
2022-03-15 17:33:28 -0300

    Implemented form fields interface

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 
-const MaterialComponents = [MatCardModule, MatButtonModule];
+const MaterialComponents = [
+  MatCardModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatIconModule,
+];
 
 @NgModule({ exports: [MaterialComponents] })
 export class MaterialModule {}
