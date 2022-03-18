c4d31c26e23095bdb188493f6d2b7460db8968e0
henriquechagasg <henriquechagasg@gmail.com>
2022-03-18 15:17:46 -0300

    Implemented more unit tests to FormComponent

--- src/app/home/form/form.component.spec.ts
+++ src/app/home/form/form.component.spec.ts
@@ -5,15 +5,69 @@ import { of } from 'rxjs';
 import { ProgressBarService } from 'src/app/core/services/progress-bar.service';
 import { FormComponent } from './form.component';
 
-export class MatDialogMock {
-  open() {
-    return {
-      afterClosed: () => of({ action: true }),
-    };
-  }
+interface StickersFormTest {
+  stickers: {
+    react: boolean;
+    vue: boolean;
+    angular: boolean;
+  };
+  quantity: number | string | null;
+  comment: string;
 }
 
+type FormConditionOptions =
+  | 'noCheckBoxSelection'
+  | 'emptyQuantity'
+  | 'negativeQuantity'
+  | 'notNumberQuantity';
+
+export class MatDialogMock {}
+
 export class MatSnackBarMock {}
+
+const initialFormValue: StickersFormTest = {
+  stickers: {
+    react: false,
+    vue: false,
+    angular: false,
+  },
+  quantity: 1,
+  comment: '',
+};
+
+const validFormValue: StickersFormTest = {
+  stickers: {
+    react: true,
+    vue: true,
+    angular: true,
+  },
+  quantity: 1,
+  comment: '',
+};
+
+const formConditions: Record<FormConditionOptions, StickersFormTest> = {
+  noCheckBoxSelection: {
+    ...validFormValue,
+    stickers: {
+      react: false,
+      vue: false,
+      angular: false,
+    },
+  },
+  emptyQuantity: {
+    ...validFormValue,
+    quantity: null,
+  },
+
+  negativeQuantity: {
+    ...validFormValue,
+    quantity: -1,
+  },
+  notNumberQuantity: {
+    ...validFormValue,
+    quantity: 'NaN',
+  },
+};
 
 describe('FormComponent', () => {
   let component: FormComponent;
@@ -38,5 +92,69 @@ describe('FormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('stickers form should initialize with initial form value', () => {
+    expect(component.stickersForm.value).toEqual(initialFormValue);
+  });
+
+  it('stickers form should initialize as invalid', () => {
+    expect(component.stickersForm.valid).toBeFalse();
+  });
+
+  it('stickers form should be invalid when any checkboxes are selected', () => {
+    component.stickersForm.patchValue(formConditions.noCheckBoxSelection);
+    expect(component.stickersForm.valid).toBeFalse();
+  });
+
+  it('stickers form should be invalid when any checkboxes are selected', () => {
+    component.stickersForm.patchValue(formConditions.noCheckBoxSelection);
+    expect(component.stickersForm.valid).toBeFalse();
+  });
+
+  it('stickers form should be invalid when quanity value is empty', () => {
+    component.stickersForm.patchValue(formConditions.emptyQuantity);
+    expect(component.stickersForm.valid).toBeFalse();
+  });
+
+  it('stickers form should be invalid when quanity value is negative', () => {
+    component.stickersForm.patchValue(formConditions.negativeQuantity);
+    expect(component.stickersForm.valid).toBeFalse();
+  });
+
+  it('stickers form should be invalid when quanity value is not a number', () => {
+    component.stickersForm.patchValue(formConditions.notNumberQuantity);
+    expect(component.stickersForm.valid).toBeFalse();
+  });
+
+  it('increment method should increment quantity value', () => {
+    expect(component.quantity.value).toBe(1);
+    component.increment();
+    expect(component.quantity.value).toBe(2);
+  });
+
+  it('decrement method should decrement quantity value', () => {
+    expect(component.quantity.value).toBe(1);
+    component.increment();
+    component.decrement();
+    expect(component.quantity.value).toBe(1);
+  });
+
+  it('quantity value should not decrement if it"s value is equal to 1', () => {
+    expect(component.quantity.value).toBe(1);
+    component.decrement();
+    expect(component.quantity.value).toBe(1);
+  });
+
+  it('quantity value should be 1 on first increment when it"s value is negative', () => {
+    component.stickersForm.patchValue({ quantity: -10 });
+    component.increment();
+    expect(component.quantity.value).toBe(1);
+  });
+
+  it('quantity value should be 1 on first increment when it"s value is not of type "number"', () => {
+    component.stickersForm.patchValue({ quantity: 'NaN' });
+    component.increment();
+    expect(component.quantity.value).toBe(1);
   });
 });
